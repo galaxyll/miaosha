8b63a27d34df7c1b2f218f3a1a89f4e17382b8ad
galaxyll <2533140373@qq.com>
2020-02-10 21:07:01 +0800

    add:md5模块

---
+++ src/main/java/com/yxh/miaosha/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.yxh.miaosha.controller;
+
+/**
+ * @author galaxy
+ * @date 20-2-10 - 下午9:06
+ */
+public class LoginController {
+}
---
+++ src/main/java/com/yxh/miaosha/util/MD5Util.java
@@ -0,0 +1,30 @@
+package com.yxh.miaosha.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * @author galaxy
+ * @date 20-2-10 - 下午8:30
+ */
+public class MD5Util {
+
+    private static String salt = "tyl687o8p98";
+
+    private static String md5(String src){
+        return DigestUtils.md5Hex(src);
+    }
+    public static String inputPassToFormPass(String inputPass){
+        String src = ""+salt.charAt(0)+salt.charAt(2) + inputPass +salt.charAt(5) + salt.charAt(4);
+        return md5(src);
+    }
+    public static String formPassToDBPass(String formPass, String salt) {
+        String str = ""+salt.charAt(0)+salt.charAt(2) + formPass +salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+
+    public static String inputPassToDBPass(String inputPass, String saltDB) {
+        String formPass = inputPassToFormPass(inputPass);
+        String dbPass = formPassToDBPass(formPass, saltDB);
+        return dbPass;
+    }
+}
